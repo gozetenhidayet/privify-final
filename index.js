8303184ab15dffe5bfd41773f27c64c0ce10fa0f
gozetenhidayet <hynngozeten@gmail.com>
2025-06-17 20:52:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Pricify Live 🎉</h1>
-      <p>Your smart price comparison site is now live!</p>
-    </div>
-  );
-}
+delete wrong root index.js
