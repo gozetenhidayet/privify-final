4294d2b757d060da641e509bd2518c82652a7d0b
Hidayet Gozeten <hynngozeten@gmail.com>
2025-06-17 19:23:37 -0500

    clean push without node_modules

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Pricify Live 🎉</h1>
+      <p>Your smart price comparison site is now live!</p>
+    </div>
+  );
+}
---
+++ pages/pricing.js
@@ -0,0 +1,8 @@
+export default function Pricing() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Pricing Plans</h1>
+      <p>Choose monthly or yearly.</p>
+    </div>
+  );
+}
