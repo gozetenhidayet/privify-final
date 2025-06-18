819ff20adf1b774946690d408592e46e8478dd85
gozetenhidayet <hynngozeten@gmail.com>
2025-06-17 20:39:09 -0500

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Admin Panel</h1>
+      <p>Only accessible by admin email.</p>
+    </div>
+  );
+}
