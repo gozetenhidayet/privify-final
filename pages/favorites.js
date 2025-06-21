0431cea7d1bbbbb4e286050685f400e792c3daca
gozetenhidayet <hynngozeten@gmail.com>
2025-06-20 20:03:35 -0500

    Create favorites.js

---
+++ pages/favorites.js
@@ -0,0 +1,13 @@
+// pages/favorites.js
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Favorites</h1>
+      <p>Your favorite products will appear here.</p>
+    </div>
+  );
+};
+
+export default Favorites;
